42c0b4eacc07fa7c7eb99cfafc7758c4844dab36
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-10-10 10:21:45 +0200

    Added exceptions

--- json_validator/exceptions.py
+++ json_validator/exceptions.py
@@ -1,5 +1,35 @@
-import exceptions
+from exceptions import Exception
 
 
 class JsonValidatorException(Exception):
-    
+    """
+    Base exception for json_validator
+    """
+
+    def __init__(self, message):
+        """
+        A short description.
+
+        A bit longer description.
+
+            :param message: description
+
+        Returns:
+            type: description
+
+        Raises:
+            Exception: description
+
+        """
+
+        super(JsonValidatorException, self).__init__(message)
+
+
+class JsonValidatorSchemaDefinitionException(JsonValidatorException):
+    def __init__(self, message):
+        super(JsonValidatorSchemaDefinitionException, self).__init__(message)
+
+
+class JsonValidatorValidationException(JsonValidatorException):
+    def __init__(self, message):
+        super(JsonValidatorValidationException, self).__init__(message)
