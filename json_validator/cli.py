889560724e02df3fa33bca3256d955bcd327dabc
Kristoffer Andersson <kode.kristoff@gmsil.com>
2018-10-10 17:18:54 +0200

    working

--- json_validator/cli.py
+++ json_validator/cli.py
@@ -2,15 +2,20 @@ import json
 
 import click
 
-from json_validator import get_validator
+import json_validator
 
 @click.command()
 @click.option('--schema', '-s', type=click.File('r'), help='Schema to use for validating.')
-@click.argument('file', default='world', required=True, type=click.File('r'))
-def main(file, schema):
+@click.argument('file_in', required=True, type=click.File('r'))
+@click.argument('file_out', required=True, type=click.File('w'))
+def main(file_in, file_out, schema):
     """Validates a json-file with a schema (json-schema.org)."""
 
-    click.echo('Validating {0} with the schema in {1}.'.format(file, schema))
-    validator = get_validator(schema)
-    data = json.load(file)
-    validator.validate(data)
+#    click.echo('Validating {0} with the schem8a88 in {1}.'.format(file, schema))
+    schema_json = json.load(schema)
+    data = json.load(file_in)
+    
+    correct, errors = json_validator.validate(schema_json, data)
+    json.dump(correct, file_out)
+    if errors:
+        print(json.dumps(errors))
