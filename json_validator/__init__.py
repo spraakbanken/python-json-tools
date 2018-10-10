fc663b919421aa8e654f41d174384496861b41f9
Kristoffer Andersson <kristoffer.andersson@gu.se>
2018-10-10 10:21:36 +0200

    Added error_id

--- json_validator/__init__.py
+++ json_validator/__init__.py
@@ -7,6 +7,7 @@ def validate(data, schema):
     except fastjsonschema.JsonSchemaDefinitionException as e:
         raise e
     errors = []
+    error_id = 0
     correct = []
 
     for obj in data:
@@ -14,9 +15,12 @@ def validate(data, schema):
             new_data = validate_entry(obj)
         except fastjsonschema.JsonSchemaException as e:
             errors.append({
+                "error_id": error_id,
                 "error": e.message,
                 "object": obj
             })
+            correct.append({"_JSON_VALIDATOR_ERROR_ID": error_id})
+            error_id += 1
         else:
             correct.append(new_data)
     return (correct, errors)
