8e5ea951e16e9b0a655ad69dbb73ba5732f050fe
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-03-18 08:18:34 +0100

    add more tests for diff

--- tests/diff/test_compare.py
+++ tests/diff/test_compare.py
@@ -3,21 +3,39 @@ import pytest
 from json_tools import diff
 
 objs = [
-{
+{ # 0
 	'a': 1
 },
-{
+{ # 1
     'a': 2
 },
-{
+{ # 2
     'a': 1,
     'b': 1
+},
+{ # 3
+    'a': '1'
+},
+{ # 4
+    'a': {
+        's': [1]
+    }
+},
+{ # 5
+    'a': {
+        's': [1,2]
+    }
+},
+{ # 6
+    'a': {
+        's': '1'
+    }
 },
 ]
 
 indices = [i for i in range(len(objs))]
 
-def change(before, after, field):
+def change(field, before, after):
     return {
         'before': before,
         'after': after,
@@ -26,7 +44,16 @@ def change(before, after, field):
     }
 
 
-def added(after, field):
+def typechange(field, before, after):
+    return {
+        'before': before,
+        'after': after,
+        'field': field,
+        'type': 'TYPECHANGE'
+    }
+
+
+def added(field, after):
     return {
         'after': after,
         'field': field,
@@ -34,7 +61,7 @@ def added(after, field):
     }
 
 
-def removed(before, field):
+def removed(field, before):
     return {
         'before': before,
         'field': field,
@@ -42,34 +69,42 @@ def removed(before, field):
     }
 
 
-facit = [
-    [
-        [] for _ in indices
-    ] for _ in indices
+_data_test_compare = [
+    (0,1,[change('a',1,2)]),
+    (0,2,[added('b',1)]),
+    (0,3,[
+        typechange('a','int','str'),
+        change('a',1,'1'),
+    ]),
+    (0,4,[
+        typechange('a', 'int','dict'),
+        change('a', 1, {'s': [1]}),
+    ]),
+    (1,0,[change('a',2,1)]),
+    (1,2,[change('a',2,1),added('b',1)]),
+    (2,0,[removed('b',1)]),
+    (2,1,[change('a',1,2), removed('b',1)]),
+    (4,5,[
+        added('a.s[1]', 2),
+    ]),
+    (4,6,[
+        typechange('a.s', 'list', 'str'),
+        {},
+        {},
+        {},
+    ]),
 ]
+for i in range(len(objs)):
+    _data_test_compare.append((i, i, []),)
 
-
-facit[0][1] = [change(1,2,'a')]
-facit[0][2] = [added(1,'b')]
-facit[1][0] = [change(2,1,'a')]
-facit[1][2] = [change(2,1,'a'),added(1,'b')]
-facit[2][0] = [removed(1,'b')]
-facit[2][1] = [change(1,2,'a'),removed(1,'b')]
-
-
-def test_setup():
-    assert len(objs) == len(indices)
-    assert len(facit) == len(indices)
-    for row in facit:
-        assert len(row) == len(indices)
-
-
-@pytest.mark.parametrize('i2', indices)
-@pytest.mark.parametrize('i1', indices)
-def test_compare(i1, i2):
+@pytest.mark.parametrize(
+    'i1,i2,facit',
+    _data_test_compare
+)
+def test_compare(i1, i2, facit):
     r = diff.compare(objs[i1], objs[i2])
 
-    assert len(r) == len(facit[i1][i2])
+    assert len(r) == len(facit)
 
-    for a, b in zip(r, facit[i1][i2]):
+    for a, b in zip(r, facit):
         assert a == b
