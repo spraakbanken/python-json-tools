72cbaaa3172bbf49025cb5e83dc04c181788c4b3
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-10-03 12:11:41 +0200

    add docstrings

--- sb_json_tools/jsonlib.py
+++ sb_json_tools/jsonlib.py
@@ -1,14 +1,33 @@
+"""
+Utility library to load the underlying json library.
+
+Imports `ujson` if it is present, otherwise it imports `json` from
+the standard library.
+"""
 try:
-    from ujson import dump, dumps, load, loads  # noqa: F401
+    from ujson import dump, dumps, load, loads  # pylint: disable=unused-import
 except ImportError:
     from json import dump, dumps, load, loads  # noqa: F401
 
 
 def load_from_file(filename: str):
+    """
+    Load the JSON file with the given filename.
+
+    :param file_name: name of the file to load from.
+    :return: the loaded JSON file.
+    """
     with open(filename, 'r') as fp:
         return load(fp)
 
 
 def dump_to_file(obj, filename: str):
+    """
+    Dump to a JSON file with the given file name.
+
+    :param obj: the object to dump.
+    :param file_name: name of the file to dump to.
+    :return: anything returned from the backend.
+    """
     with open(filename, 'w') as fp:
         return dump(obj, fp)
