d8d225a78df200b4a56780a986523a23e902d7f9
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-12-11 13:08:40 +0100

    update cli

--- sb_json_tools/cli.py
+++ sb_json_tools/cli.py
@@ -7,7 +7,7 @@ from sb_json_tools import jt_val
 from sb_json_tools import jt_iter
 
 
-__version__ = '0.4.0'
+__version__ = "0.4.0"
 
 
 @click.group()
@@ -17,8 +17,8 @@ def cli():
 
 @cli.command()
 @click.option("--schema", "-s", help="Schema to use for validating.")
-@click.argument("infile", type=click.File('r'))
-@click.argument("outfile", type=click.File('w'))
+@click.argument("infile", type=click.File("r"))
+@click.argument("outfile", type=click.File("w"))
 def validate(infile, outfile, schema):
     """Validates a json-file with a schema (json-schema.org)."""
     # click.echo("Validating {0} with the schema in {1}.".format(infile, schema))
@@ -36,14 +36,11 @@ def validate(infile, outfile, schema):
 
 
 @cli.command()
-@click.argument('src', type=click.File('r'))
-@click.argument('dst', type=click.File('w'))
+@click.argument("src", type=click.File("r"))
+@click.argument("dst", type=click.File("w"))
 def convert(src, dst):
-    jt_iter.dump(
-        jt_iter.load(src),
-        dst
-    )
+    jt_iter.dump(jt_iter.load(src), dst)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
--- sb_json_tools/utils.py
+++ sb_json_tools/utils.py
@@ -1,7 +1,20 @@
+"""Util functions.
+"""
 import os
 
 
-def is_jsonl(p: str):
-    root, suffix = os.path.splitext(p)
+def is_jsonl(name: str) -> bool:
+    """Test if a filename is json lines.
+    Also returns True for '<stdin>' and '<stdout>'.
+
+    Arguments:
+        name {str} -- name to test
+
+    Returns:
+        {bool} -- True if this name is jsonl.
+    """
+    if name in ["<stdin>", "<stdout>"]:
+        return True
+    _, suffix = os.path.splitext(name)
     # print('suffix = {suffix}'.format(suffix=suffix))
-    return suffix in ['.jsonl']
+    return suffix in [".jsonl"]
